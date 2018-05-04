a33c01266bb92a7ee27dd6a3c3efcdcb822971b3
Joel Salisbury <udumas@rochester.rr.com>
2018-05-04 01:34:56 -0400

    renamed ex1 to ex6 improperly named before.

--- ex6.py
+++ ex6.py
@@ -3,7 +3,7 @@ x = f"There are {types_of_people} types of people."
 
 binary = "binary"
 do_not = "don't"
-y = f"Those who {binary} and those who {do_not}."
+y = f"Those who know {binary} and those who {do_not}."
 
 
 print(x)
