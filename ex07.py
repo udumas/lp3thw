ad1d773228520400c65fb5f85273ce3462ba1933
Joel Salisbury <udumas@users.noreply.github.com>
2019-05-17 19:57:18 -0400

    added comments

--- ex07.py
+++ ex07.py
@@ -1,8 +1,15 @@
+#print Mary had a little lamb
 print("Mary had a little lamb")
+#print It's fleece was white as snow inserting snow in place of the {}
 print("It's fleece was white as {}.".format('snow'))
+#print and everywhere that mary went
 print("And everywhere that Mary went.")
+#print ten .s
 print("." * 10) # what'd that do?
 
+#create variables end1 end2 end3 end4 end5 
+#end6 end7 end8 end9 end10 end11
+#end12
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +24,7 @@ end11 = "e"
 end12 = "r"
 
 # watch that comma at the end. try removing it to see what happens
+#print cheese and a space
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+#print burger
 print(end7 + end8 + end9 + end10 + end11 + end12)
