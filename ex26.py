075570e2b2fdd885edc9570011dd845fe0a7341b
Joel Salisbury <udumas@users.noreply.github.com>
2019-05-18 21:58:03 -0400

    corrected more typos

--- ex26.py
+++ ex26.py
@@ -63,14 +63,14 @@ print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secret_formula(startpoint)
+formula = secret_formula(start_point)
 # this is an easy way to apply a list to a format string
 print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
 
 
 
 people = 20
-crates = 30
+cats = 30
 dogs = 15
 
 
