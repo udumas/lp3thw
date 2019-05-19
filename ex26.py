728b85a4a6b5e756c880771e3b63c0510f7a72d6
Joel Salisbury <udumas@users.noreply.github.com>
2019-05-18 21:45:07 -0400

    another missing if colon

--- ex26.py
+++ ex26.py
@@ -89,7 +89,7 @@ dogs += 5
 if people >= dogs:
     print("People are greater than or equal to dogs.")
 
-if people <= dogs
+if people <= dogs:
     print("People are less than or equal to dogs.)
 
 
