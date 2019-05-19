0b1d65f6a2475f9fcdd15fa88837aab4c4a5f3ec
Joel Salisbury <udumas@users.noreply.github.com>
2019-05-18 21:33:49 -0400

    corrected typos

--- ex26.py
+++ ex26.py
@@ -11,7 +11,7 @@ script, filename = argv
 txt = open(filenme)
 
 print("Here's your file {filename}:")
-print(tx.read())
+print(txt.read())
 
 print("Type the filename again:")
 file_again = input("> ")
@@ -21,7 +21,7 @@ txt_again = open(file_again)
 print(txt_again_read())
 
 
-print('Let's practice everything.')
+print("Let's practice everything.")
 print('You\'d need to know \'bout escapes 
       with \\ that do \n newlines and \t tabs.')
 
