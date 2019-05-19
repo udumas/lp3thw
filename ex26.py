8a4687d17cca35d6238113c2ee9801d25a4ed6ae
Joel Salisbury <udumas@users.noreply.github.com>
2019-05-18 22:01:04 -0400

    changed * to / to repair damaged logic

--- ex26.py
+++ ex26.py
@@ -48,7 +48,7 @@ print(f"This should be five: {five}")
 def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
-    crates = jars * 100
+    crates = jars / 100
     return jelly_beans, jars, crates
 
 
