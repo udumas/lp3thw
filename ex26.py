7b39a6fcec91057eee7c2af45bc01d208562f958
Joel Salisbury <udumas@users.noreply.github.com>
2019-05-18 21:40:19 -0400

    missing colon at beginning of function definition

--- ex26.py
+++ ex26.py
@@ -42,7 +42,7 @@ print("--------------")
 five = 10 - 2 + 3 - 6
 print(f"This should be five: {five}")
 
-def secret_formula(started)
+def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
     crates = jars  100
