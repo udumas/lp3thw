c0a7a20a0e026411bf03804f9bd4e78c0bc1ad18
Joel Salisbury <udumas@users.noreply.github.com>
2019-05-23 01:08:52 -0400

    fixed  typo

--- ex29.py
+++ ex29.py
@@ -3,7 +3,7 @@ cats = 30
 dogs = 15
 
 if people < cats:
-    print("Not many cats! The world is doomed")
+    print("Too many cats! The world is doomed")
 
 if people > cats:
     print("Not many cats! The world is saved!")
