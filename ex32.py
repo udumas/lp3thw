549d69b3177dc8070c01844f1b5797680dd1652a
Joel Salisbury <udumas@rochester.rr.com>
2019-05-23 01:06:19 -0400

    added lines to ex32.py

---
+++ ex32.py
@@ -0,0 +1,7 @@
+the_count = [1,2,3,4,5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1,'pennies', 2, 'dimes',3,'quarters']
+
+# this first kind of for loop goes through a list
+for number in count:
+    #save as above
