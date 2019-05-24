d57db94677903895de587a3aeff4beeab60d9150
Joel Salisbury <udumas@rochester.rr.com>
2019-05-24 16:57:45 -0400

    refactored ex33.py to use a function that accepts an integer value  and adds that number of elements to the list numbers.

--- ex33.py
+++ ex33.py
@@ -1,14 +1,16 @@
-i = 0
-numbers = []
+the_value = 9
+def while_fun(the_value):
+    i=0
+    numbers = []
+    while i < the_value:
+        print(f"At the top i is {i}")
+        numbers.append(i)
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
-
-    i+=1
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
-
+        i+=1
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
+    return numbers
+numbers = while_fun(the_value)
 print("The Numbers: ")
 
 for num in numbers:
